07a9efd91273fca4b9fe405abfe6918308637aa6
itsinfi <sascha.angermann2002@gmail.com>
2025-04-25 18:05:07 +0200

    work on create your first android app tutorial

--- app/src/main/java/com/example/test/MainActivity.kt
+++ app/src/main/java/com/example/test/MainActivity.kt
@@ -8,9 +8,12 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.test.ui.theme.TestTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,16 +35,19 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Surface(color = Color.DarkGray) {
+        Text(
+            text = "Hello $name!",
+            modifier = modifier.padding(24.dp),
+            color = Color.White
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     TestTheme {
-        Greeting("Android")
+        Greeting("Sascha")
     }
 }
